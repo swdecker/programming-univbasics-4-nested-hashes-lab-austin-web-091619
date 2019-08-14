1c4c76fa15125c0866e30111c244a8073933ea74
Steven Decker <swdecker@gmail.com>
2019-08-14 03:46:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -67,6 +68,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		    
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
